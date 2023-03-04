72d602febfe4413c655d94b75cce1705f11fcd04
Sebastian009w <ia3266505@gmail.com>
2023-03-04 14:17:59 -0500

    schemas mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskController } from './task/task.controller';
 import { TaskService } from './task/task.service';
-import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
